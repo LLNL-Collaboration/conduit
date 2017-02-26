47f61b36769855578e33e909dd03a874738f4220
Averrie <xhuang53@illinois.edu>
2017-02-25 20:30:30 -0600

    CMakeLists.txt

---
+++ src/tests/relay/t_relay_blueprint_websocket.cpp
@@ -0,0 +1,118 @@
+#include "conduit_relay.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "gtest/gtest.h"
+
+#include "t_config.hpp"
+
+using namespace conduit;
+using namespace conduit::utils;
+using namespace conduit::relay;
+
+
+bool launch_server = false;
+bool use_ssl       = false;
+bool use_auth      = false;
+
+std::string blueprint_path;
+
+TEST(conduit_relay_web_websocket, websocket_test)
+{
+    if(! launch_server)
+    {
+        return;
+    }
+
+    std::string wsock_path = utils::join_file_path(web::web_client_root_directory(),
+                                                   "blueprint_websocket");
+
+    std::string example_blueprint_mesh_path = utils::join_file_path(wsock_path,
+                                                         "blueprint.json");
+
+    std::ifstream blueprint(example_blueprint_mesh_path);
+    std::stringstream buffer;
+    buffer << blueprint.rdbuf();
+
+    Node blueprint_node;
+    Generator g(buffer.str(), "json", NULL);
+    g.walk(blueprint_node);
+    EXPECT_EQ(blueprint_node.fetch("topologies/mesh/path").to_json(),
+                                                     "\"topologies/mesh\"");
+    blueprint_node.to_json_stream("test.json","json");
+
+
+    // setup the webserver
+    web::WebServer svr;
+
+    if(use_ssl)
+    {
+        std::string cert_file = utils::join_file_path(CONDUIT_T_SRC_DIR,"relay");
+        cert_file = utils::join_file_path(cert_file,"t_ssl_cert.pem");
+        svr.set_ssl_certificate_file(cert_file);
+    }
+
+    if(use_auth)
+    {
+        std::string auth_file = utils::join_file_path(CONDUIT_T_SRC_DIR,"relay");
+        auth_file = utils::join_file_path(auth_file,"t_htpasswd.txt");
+        svr.set_htpasswd_auth_domain("test");
+        svr.set_htpasswd_auth_file(auth_file);
+    }
+
+    svr.set_port(8081);
+    svr.set_document_root(wsock_path);
+
+    svr.serve();
+
+    while(svr.is_running()) 
+    {
+        utils::sleep(1000);
+        
+        // websocket() returns the first active websocket
+        svr.websocket()->send(blueprint_node);
+        // or with a very short timeout
+        //svr.websocket(10,100)->send(msg);
+        
+    }
+}
+
+
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    int result = 0;
+    std::cout << argc << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    
+    for(int i = 0; i < argc; i++)
+    {
+        std::string arg_str(argv[i]);
+        if(arg_str == "launch")
+        {
+            // actually launch the server
+            launch_server = true;
+        }
+        else if(arg_str == "ssl")
+        {
+            // test using ssl server cert
+            use_ssl = true;
+        }
+        else if(arg_str == "auth")
+        {
+            // test using htpasswd auth
+            // the user name and password for this example are both "test"
+            use_auth = true;
+        }
+	else if (arg_str == "-p")
+	{
+	    blueprint_path = std::string(argv[i + 1]);
+	}
+    }
+
+    result = RUN_ALL_TESTS();
+    return result;
+}
+
+
